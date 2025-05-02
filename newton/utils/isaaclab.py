97564fcd0413da029a1bf289d9aac9ed04a5f66f
Lukasz Wawrzyniak <lwawrzyniak@nvidia.com>
2025-05-02 15:36:56 -0400

    Add utility to replicate Isaac Lab environments with example

---
+++ newton/utils/isaaclab.py
@@ -0,0 +1,98 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 The Newton Developers
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import warp as wp
+
+import newton
+from newton.examples import compute_env_offsets
+
+
+def replicate_environment(source, prototype_path, path_pattern, num_envs, env_spacing, **usd_kwargs):
+    """
+    Replicates a prototype USD environment in Newton.
+
+    Args:
+        source (str | pxr.UsdStage): The file path to the USD file, or an existing USD stage instance.
+        prototype_path (str): The USD path where the prototype env is defined, e.g., "/World/envs/env_0".
+        path_pattern (str): The USD path pattern for replicated envs, e.g., "/World/envs/env_{}".
+        num_envs (int): Number of replicas to create.
+        env_spacing (tuple[float]): Environment spacing vector.
+        **usd_kwargs: Keyword arguments to pass to the USD importer (see `newton.utils.parse_usd()`).
+
+    Returns:
+        (newton.ModelBuilder, dict): The resulting ModelBuilder containing all replicated environments and a dictionary with USD stage information.
+    """
+
+    # first, load everything except the prototype env
+    builder = newton.ModelBuilder()
+    stage_info = newton.utils.parse_usd(
+        source,
+        builder,
+        ignore_paths=[prototype_path],
+        **usd_kwargs,
+    )
+
+    # now, load just the prototype env
+    prototype_builder = newton.ModelBuilder()
+    newton.utils.parse_usd(
+        source,
+        prototype_builder,
+        root_path=prototype_path,
+        **usd_kwargs,
+    )
+
+    up_axis = stage_info.get("up_axis") or "Z"
+
+    env_offsets = compute_env_offsets(num_envs, env_offset=env_spacing, up_axis=up_axis)
+
+    # clone the prototype env with updated paths
+    for i in range(num_envs):
+        body_start = builder.body_count
+        shape_start = builder.shape_count
+        joint_start = builder.joint_count
+        articulation_start = builder.articulation_count
+
+        builder.add_builder(prototype_builder, xform=wp.transform(env_offsets[i], wp.quat_identity()))
+
+        if i > 0:
+            update_paths(
+                builder,
+                prototype_path,
+                path_pattern.format(i),
+                body_start=body_start,
+                shape_start=shape_start,
+                joint_start=joint_start,
+                articulation_start=articulation_start,
+            )
+
+    return builder, stage_info
+
+
+def update_paths(
+    builder, old_root, new_root, body_start=None, shape_start=None, joint_start=None, articulation_start=None
+):
+    old_len = len(old_root)
+    if body_start is not None:
+        for i in range(body_start, builder.body_count):
+            builder.body_key[i] = f"{new_root}{builder.body_key[i][old_len:]}"
+    if shape_start is not None:
+        for i in range(shape_start, builder.shape_count):
+            builder.shape_key[i] = f"{new_root}{builder.shape_key[i][old_len:]}"
+    if joint_start is not None:
+        for i in range(joint_start, builder.joint_count):
+            builder.joint_key[i] = f"{new_root}{builder.joint_key[i][old_len:]}"
+    if articulation_start is not None:
+        for i in range(articulation_start, builder.articulation_count):
+            builder.articulation_key[i] = f"{new_root}{builder.articulation_key[i][old_len:]}"
